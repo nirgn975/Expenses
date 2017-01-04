819111b6219639a8169a7518e4022a6927d5498c
Nir Galon <nir@galon.io>
2017-01-04 20:46:15 +0200

    Build CRUD operations for Category model (#12)
    * Add CRUD operations
    
    * Fix eslint error.

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('./categoryController');
 
+router.param('id', controller.param);
+
 router.route('/')
   .get(controller.get)
   .post(controller.post);
