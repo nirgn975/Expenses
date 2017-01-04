6e02dd927bcf616db1ffa731d7eaca11090ce227
nirgn975 <nir@galon.io>
2017-01-04 21:30:54 +0200

    Add CRUD operations

--- server/api/budget/budgetController.js
+++ server/api/budget/budgetController.js
@@ -1,22 +1,70 @@
 const Budget = require('./budgetModel');
 const _ = require('lodash');
+const logger = require('../../util/logger');
+
+exports.param = (res, req, next, id) => {
+  Budget.findById(id)
+    .populate('categories')
+    .then((budget) => {
+      if (!budget) {
+        next(new Error(`No budget with that id: ${id}`));
+      } else {
+        req.budget = budget;
+        next();
+      }
+    }, (error) => {
+      next(error);
+    })
+};
 
 exports.get = (res, req, next) => {
-  console.log('in budget get');
+  Budget.find({})
+    .then((budgets) => {
+      res.json(budgets);
+    }, (error) => {
+      next(error);
+    });
 };
 
 exports.post = (res, req, next) => {
-  console.log('in budget post');
+  const newBudget = req.body;
+
+  Budget.create(newBudget)
+    .then((savedBudget) => {
+      res.json(savedBudget);
+    }, (error) => {
+      logger.error(error);
+      next(error);
+    });
 };
 
 exports.getOne = (res, req, next) => {
-  console.log('in budget getOne');
+  res.json(req.budget);
 };
 
 exports.put = (res, req, next) => {
-  console.log('in budget put');
+  const budget = req.budget;
+  const update = req.body;
+
+  _.merge(budget, update);
+
+  budget.save((error, saved) => {
+    if (error) {
+      next(error);
+    } else {
+      res.json(saved);
+    }
+  });
 };
 
 exports.delete = (res, req, next) => {
-  console.log('in budget delete');
+  const budget = req.body;
+
+  budget.delete((error, removed) => {
+    if (error) {
+      next(error);
+    } else {
+      res.json(removed);
+    }
+  });
 };
