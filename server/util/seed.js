1a07423a597de3590134dfb4a56ac9d0680753f7
nirgn975 <nir@galon.io>
2017-01-04 19:23:34 +0200

    Fix eslint errors in seed.js file

--- server/util/seed.js
+++ server/util/seed.js
@@ -6,31 +6,31 @@ const logger = require('./logger');
 logger.log('Seeding the Database');
 
 const transactions = [
-  {amount: 23, date: Date.now(), type: 'expense', coordinates: [21, 22], description: 'description foo bar 1'},
-  {amount: 54, date: Date.now(), type: 'income', coordinates: [21, 42], description: 'description foo bar 2'},
-  {amount: 76, date: Date.now(), type: 'expense', coordinates: [15, 12], description: 'description foo bar 3'},
-  {amount: 1009, date: Date.now(), type: 'income', coordinates: [-23, 76], description: 'description foo bar 4'},
+  { amount: 23, date: Date.now(), type: 'expense', coordinates: [21, 22], description: 'description foo bar 1' },
+  { amount: 54, date: Date.now(), type: 'income', coordinates: [21, 42], description: 'description foo bar 2' },
+  { amount: 76, date: Date.now(), type: 'expense', coordinates: [15, 12], description: 'description foo bar 3' },
+  { amount: 1009, date: Date.now(), type: 'income', coordinates: [-23, 76], description: 'description foo bar 4' },
 ];
 
 const categories = [
-  {name: 'Salary', icons: 'salary'},
-  {name: 'Food', icons: 'food'},
-  {name: 'Clothing', icons: 'clothing'},
-  {name: 'Shopping', icons: 'shopping'},
+  { name: 'Salary', icons: 'salary' },
+  { name: 'Food', icons: 'food' },
+  { name: 'Clothing', icons: 'clothing' },
+  { name: 'Shopping', icons: 'shopping' },
 ];
 
-const cleanDB = function() {
+const cleanDB = () => {
   logger.log('... cleaning the DB');
-    const cleanPromises = [Category, Transaction]
+  const cleanPromises = [Category, Transaction]
     .map((model) => {
       return model.remove().exec();
     });
   return Promise.all(cleanPromises);
 };
 
-const createDoc = (model, doc) => {
+const createDoc = (Model, doc) => {
   return new Promise((resolve, reject) => {
-    new model(doc).save((err, saved) => {
+    new Model(doc).save((err, saved) => {
       return err ? reject(err) : resolve(saved);
     });
   });
@@ -38,35 +38,23 @@ const createDoc = (model, doc) => {
 
 const createCategories = (data) => {
   const promises = categories.map((category) => {
-      return createDoc(Category, category);
+    return createDoc(Category, category);
   });
 
   return Promise.all(promises)
-      .then((categories) => {
-          return _.merge({categories: categories}, data || {});
-      });
+    .then((savedCategories) => {
+      return _.merge({ categories: savedCategories }, data || {});
+    });
 };
 
 const createTransactions = (data) => {
-  const addCategory = (transaction, category) => {
-    transaction.category.push(category);
-
-    return new Promise((resolve, reject) => {
-      transaction.save((err, saved) => {
-        return err ? reject(err) : resolve(saved);
-      });
-    });
-  };
-
   const newTransactions = transactions.map((transaction, i) => {
     transaction.category = data.categories[i]._id;
     return createDoc(Transaction, transaction);
   });
 
   return Promise.all(newTransactions)
-    .then(function() {
-      return 'Seeded DB with 3 Transactions 3 Categories';
-    });
+    .then(() => 'Seeded DB with 3 Transactions 3 Categories');
 };
 
 
