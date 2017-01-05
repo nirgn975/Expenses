bf153cf1ec7bf1d77d7263a8fdcbb3f7e0f4a41c
nirgn975 <nir@galon.io>
2017-01-05 17:30:10 +0200

    Edit Facebook config

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,11 @@ const config = {
   port: process.env.PORT || 3000,
   expireTime: 24 * 60 * 10, // 10 days in minutes
   secrets: {
-    jwt: process.env.JWT || 'gumball',
+    facebookAuth: {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
+    },
   },
 };
 
