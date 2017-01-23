a5feb59536ac78f90693bd09e21247c00048581e
Nir Galon <nir@galon.io>
2017-01-23 13:03:47 +0200

    Add dependencies (#34)
    * Add dependencies and configure Angular Material
    
    * Change app to exp
    
    * Fix e2e tests and configure material theme

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('expenses App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('exp works!');
   });
 });
