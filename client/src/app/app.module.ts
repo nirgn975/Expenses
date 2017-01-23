b3346b5f4af89e7cb832dc65c3f8fe1fb6f35f90
nirgn975 <nir@galon.io>
2017-01-23 13:23:34 +0200

    Add hammerjs and angular flex-layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,25 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ExpComponent } from './exp.component';
 
 @NgModule({
   declarations: [
-    ExpComponent
+    ExpComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
+    FlexLayoutModule.forRoot(),
   ],
   providers: [],
   bootstrap: [ExpComponent]
