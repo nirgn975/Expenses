22aa47acccc39c8f429b74b081a4d48b7a4e5514
nirgn975 <nir@galon.io>
2017-01-23 16:04:13 +0200

    Cleanup

--- client/src/app/exp.component.ts
+++ client/src/app/exp.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { MdSidenavContainer, MdSidenav } from '@angular/material/sidenav';
-import { MdIcon } from '@angular/material/icon';
-import { MdMenu } from '@angular/material/menu';
-import { MdToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'exp-root',
@@ -10,5 +6,4 @@ import { MdToolbar } from '@angular/material/toolbar';
   styleUrls: ['./exp.component.scss']
 })
 export class ExpComponent {
-  title = 'exp works!';
 }
