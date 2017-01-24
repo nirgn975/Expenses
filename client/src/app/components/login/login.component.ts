eab495b69ce59e67d2e81be4a444d6fcdf22e7d2
nirgn975 <nir@galon.io>
2017-01-24 21:03:43 +0200

    Style the login screen

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'exp-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  host: {'class': 'login-view'}
 })
 export class LoginComponent implements OnInit {
 
