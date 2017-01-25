823e64150575c28e8fee12f172c2d98dbf3a1b2c
nirgn975 <nir@galon.io>
2017-01-25 17:37:37 +0200

    Cleanup

--- server/api/auth/authRoutes.js
+++ server/api/auth/authRoutes.js
@@ -4,9 +4,6 @@ const passport = require('passport');
 router.route('/facebook')
   .get(passport.authenticate('facebook'));
 
-// router.route('/facebook/callback')
-//   .get(passport.authenticate('facebook', { successRedirect: 'http://localhost:4200/home', failureRedirect: '/login' }));
-
 router.route('/facebook/callback')
   .get(passport.authenticate('facebook'), (req, res) => {
     if (req.user) {
