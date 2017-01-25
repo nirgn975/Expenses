6168e631c4cfe728fab7d7e3fa65a18fba4841df
nirgn975 <nir@galon.io>
2017-01-25 17:17:00 +0200

    Get user data from facebook and use token in guard

--- server/api/auth/authRoutes.js
+++ server/api/auth/authRoutes.js
@@ -2,9 +2,19 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.route('/facebook')
-  .get(passport.authenticate('facebook', { scope: 'email' }));
+  .get(passport.authenticate('facebook'));
+
+// router.route('/facebook/callback')
+//   .get(passport.authenticate('facebook', { successRedirect: 'http://localhost:4200/home', failureRedirect: '/login' }));
 
 router.route('/facebook/callback')
-  .get(passport.authenticate('facebook', { successRedirect: 'http://localhost:4200/home', failureRedirect: '/login' }));
+  .get(passport.authenticate('facebook'), (req, res) => {
+    if (req.user) {
+      res.cookie('userToken', req.user.token);
+      res.redirect('http://localhost:4200/home');
+    } else {
+      res.send(401);
+    }
+  });
 
 module.exports = router;
