0319864dff59ff8fae294b2266c296976e8e8cb5
nirgn975 <nir@galon.io>
2017-01-27 17:44:00 +0200

    Test the guard

--- client/src/app/guards/auth.guard.spec.ts
+++ client/src/app/guards/auth.guard.spec.ts
@@ -13,4 +13,33 @@ describe('AuthGuard', () => {
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
+
+  it('should not allow access without a token', inject([AuthGuard], (guard: AuthGuard) => {
+    expect(guard.canActivateChild()).toBeFalsy();
+  }));
+
+  it('should allow access when token is in local storage', inject([AuthGuard], (guard: AuthGuard) => {
+    // Set token in localStorage
+    localStorage.setItem('userToken', '12345');
+
+    expect(guard.canActivateChild()).toBeTruthy();
+  }));
+
+  it('should allow access when token is in cookie', inject([AuthGuard], (guard: AuthGuard) => {
+    // Set token in a cookie
+    document.cookie = 'userToken=12345; expires=' + new Date().toUTCString() + '; path=/';
+
+    expect(guard.canActivateChild()).toBeTruthy();
+  }));
+
+  it('should save token from cookie in local storage', () => {
+    const token = '12345';
+
+    // Set token in a cookie
+    document.cookie = 'userToken=' + token + '; expires=' + new Date().toUTCString() + '; path=/';
+
+    // Get the token from localStorage
+    const localStorageToken = localStorage.getItem('userToken');
+    expect(localStorageToken).toEqual(token);
+  });
 });
