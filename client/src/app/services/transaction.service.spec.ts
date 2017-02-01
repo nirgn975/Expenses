e4c3502b20b3e696c0407f053ab3539dc04bf738
nirgn975 <nir@galon.io>
2017-02-01 14:42:48 +0200

    Fix tests

--- client/src/app/services/transaction.service.spec.ts
+++ client/src/app/services/transaction.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TransactionService } from './transaction.service';
+import { HttpModule } from '@angular/http';
 
 describe('TransactionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [TransactionService]
     });
   });
--- client/src/app/services/transaction.service.ts
+++ client/src/app/services/transaction.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import { Transaction } from '../models/transaction';
