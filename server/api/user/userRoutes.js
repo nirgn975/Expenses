f411a06f57fc6b25f6b0bf3b01e5e4aabccb59f7
nirgn975 <nir@galon.io>
2017-02-10 22:57:30 +0200

    Add endpoints to GET user own info and edit it.

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const userController = require('./userController');
 
 router.route('/')
+  .put(userController.put)
   .post(userController.post);
+
+router.route('/me')
+  .get(userController.getOwn);
 
 module.exports = router;
