4df4d3644091a851c86234918ed51ef383023d88
nirgn975 <nir@galon.io>
2017-02-10 23:11:38 +0200

    Add test for GET and PUT user endpoints

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -11,6 +11,11 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe(chalk.blue('User'), () => {
+  let user = {
+    email: 'nir@galon.io',
+    token: '123',
+  };
+
   before((done) => {
     // Empty all the collection.
     Object.keys(mongoose.connection.collections).forEach((collectionName) => {
@@ -21,11 +26,6 @@ describe(chalk.blue('User'), () => {
   });
 
   it('should POST a new user', (done) => {
-    const user = {
-      email: 'nir@galon.io',
-      token: '123',
-    };
-
     chai.request(server)
       .post('/api/user')
       .send(user)
@@ -35,6 +35,39 @@ describe(chalk.blue('User'), () => {
         res.body.user.should.have.property('_id');
         res.body.user.should.have.property('email');
         res.body.user.should.have.property('token');
+        done();
+      });
+  });
+
+  it('should GET user info', (done) => {
+    chai.request(server)
+      .get('/api/user/me')
+      .set('token', user.token)
+      .end((error, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('_id');
+        res.body.should.have.property('email').equal(user.email);
+        res.body.should.have.property('token').equal(user.token);
+
+        // Save the user info
+        user = res.body;
+        done();
+      });
+  });
+
+  it('should PUT a user', (done) => {
+    user.twitter = {
+      name: 'nir',
+    };
+
+    chai.request(server)
+      .put('/api/user/')
+      .set('token', user.token)
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message').equal('User successfully updated!');
+        res.body.user.should.be.eql(user);
         done();
       });
   });
