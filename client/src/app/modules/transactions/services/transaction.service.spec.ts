b837a864731f52221816a14cdf6319873d0a565e
nirgn975 <nir@galon.io>
2017-02-10 20:22:42 +0200

    Fix service test, and add simple effect test

--- client/src/app/modules/transactions/services/transaction.service.spec.ts
+++ client/src/app/modules/transactions/services/transaction.service.spec.ts
@@ -28,48 +28,55 @@ describe('TransactionService', () => {
     expect(service).toBeTruthy();
   }));
 
-  // it('should GET all the transactions for the current month',
-  //   inject([TransactionService, MockBackend], (service: TransactionService, mockBackend: MockBackend) => {
-  //     const mockResponse = [{
-  //       '_id': '5891ce58165eb62d2b84760f',
-  //       'amount': 23,
-  //       'date': '2017-02-01T12:02:32.607Z',
-  //       'type': 'expense',
-  //       'description': 'description foo bar 1',
-  //       'category': '5891ce58165eb62d2b847606',
-  //       '__v': 0,
-  //       'coordinates': [
-  //         21,
-  //         22
-  //       ]
-  //     }, {
-  //       '_id': '5891ce58165eb62d2b847610',
-  //       'amount': 54,
-  //       'date': '2017-02-01T12:02:32.607Z',
-  //       'type': 'income',
-  //       'description': 'description foo bar 2',
-  //       'category': '5891ce58165eb62d2b847607',
-  //       '__v': 0,
-  //       'coordinates': [
-  //         21,
-  //         42
-  //       ]
-  //     }];
-  //
-  //     mockBackend.connections.subscribe((connection) => {
-  //       connection.mockRespond(new Response(new ResponseOptions({
-  //         body: JSON.stringify(mockResponse)
-  //       })));
-  //     });
-  //
-  //     service.getTransactions().subscribe(transactions => {
-  //       expect(transactions.length).toEqual(2);
-  //       expect(transactions[0].amount).toEqual(23);
-  //       expect(transactions[0].type).toEqual('expense');
-  //       expect(transactions[1].amount).toEqual(54);
-  //       expect(transactions[1].type).toEqual('income');
-  //     });
-  // }));
+  it('should GET all the transactions for the current month',
+    inject([TransactionService, MockBackend], (service: TransactionService, mockBackend: MockBackend) => {
+      const mockResponse = [{
+        '_id': '5891ce58165eb62d2b84760f',
+        'amount': 23,
+        'date': '2017-02-01T12:02:32.607Z',
+        'type': 'expense',
+        'description': 'description foo bar 1',
+        'category': '5891ce58165eb62d2b847606',
+        '__v': 0,
+        'coordinates': [
+          21,
+          22
+        ]
+      }, {
+        '_id': '5891ce58165eb62d2b847610',
+        'amount': 54,
+        'date': '2017-02-01T12:02:32.607Z',
+        'type': 'income',
+        'description': 'description foo bar 2',
+        'category': '5891ce58165eb62d2b847607',
+        '__v': 0,
+        'coordinates': [
+          21,
+          42
+        ]
+      }];
+
+      const time = {
+        _id: {
+          month: 2,
+          year: 2017,
+        },
+      };
+
+      mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+
+      service.getTransactionsByDate(time).subscribe(transactions => {
+        expect(transactions.length).toEqual(2);
+        expect(transactions[0].amount).toEqual(23);
+        expect(transactions[0].type).toEqual('expense');
+        expect(transactions[1].amount).toEqual(54);
+        expect(transactions[1].type).toEqual('income');
+      });
+  }));
 
   it('should GET all the months where there are transactions',
     inject([TransactionService, MockBackend], (service: TransactionService, mockBackend: MockBackend) => {
