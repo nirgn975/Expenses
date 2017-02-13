f1e59fe792e0a08774bdb7835bb702e09f7d8bc9
nirgn975 <nir@galon.io>
2017-02-13 10:39:00 +0200

    Add Momentjs to format transaction time

--- client/src/app/modules/transactions/components/transaction/transaction.component.ts
+++ client/src/app/modules/transactions/components/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
 
 import { Transaction } from '../../models/transaction';
 
@@ -7,9 +8,21 @@ import { Transaction } from '../../models/transaction';
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss']
 })
-export class TransactionComponent {
+export class TransactionComponent implements OnInit {
   @Input() transaction: Transaction;
+  private transactionDate: string;
 
   constructor() { }
 
+  ngOnInit() {
+    const originalTime = new Date(this.transaction.date);
+    const now = new Date();
+
+    // Check if the year, month and day are the same as now
+    if (originalTime.getFullYear() === now.getFullYear() && originalTime.getMonth() === now.getMonth() && originalTime.getDate() === now.getDate()) {
+      this.transactionDate = moment(this.transaction.date).fromNow();
+    } else {
+      this.transactionDate = moment(this.transaction.date).calendar();
+    }
+  }
 }
