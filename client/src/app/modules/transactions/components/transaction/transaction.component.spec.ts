fef7b18e35fc9ec5e2db1d1d33e3454cb7605eaf
nirgn975 <nir@galon.io>
2017-02-19 22:26:43 +0200

    Fix hammerjs warning in test

--- client/src/app/modules/transactions/components/transaction/transaction.component.spec.ts
+++ client/src/app/modules/transactions/components/transaction/transaction.component.spec.ts
@@ -1,4 +1,5 @@
 /* tslint:disable:no-unused-variable */
+import 'hammerjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
