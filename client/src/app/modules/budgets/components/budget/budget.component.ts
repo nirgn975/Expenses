f1010c2a0239352a2a0761855038f43e7040a168
nirgn975 <nir@galon.io>
2017-03-31 19:28:55 +0300

    Cleanup

--- client/src/app/modules/budgets/components/budget/budget.component.ts
+++ client/src/app/modules/budgets/components/budget/budget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import * as moment from 'moment';
 
 import { Budget } from '../../models/budget';
@@ -9,28 +9,9 @@ import { Budget } from '../../models/budget';
   styleUrls: ['./budget.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BudgetComponent implements OnInit {
+export class BudgetComponent {
   @Input() budget: Budget;
-  public budgetDate: string;
-  public showDetail: Boolean = false;
 
   constructor() { }
 
-  ngOnInit() {
-    // const originalTime = new Date(this.budget.date);
-    // const now = new Date();
-    // 
-    // // Check if the year, month and day are the same as now
-    // if (originalTime.getFullYear() === now.getFullYear() &&
-    //     originalTime.getMonth() === now.getMonth() &&
-    //     originalTime.getDate() === now.getDate()) {
-    //   this.budgetDate = moment(this.budget.date).fromNow();
-    // } else {
-    //   this.budgetDate = moment(this.budget.date).calendar();
-    // }
-  }
-
-  showMore($event) {
-    this.showDetail = $event;
-  }
 }
