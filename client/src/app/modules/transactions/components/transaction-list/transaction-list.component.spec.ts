b0a1c8f0fc5e45842ff68e4fd82dd1cbe126429e
nirgn975 <nir@galon.io>
2017-06-23 12:04:02 +0300

    Fix tests

--- client/src/app/modules/transactions/components/transaction-list/transaction-list.component.spec.ts
+++ client/src/app/modules/transactions/components/transaction-list/transaction-list.component.spec.ts
@@ -3,6 +3,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { MaterialModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { reducer } from '../../reducers';
@@ -13,6 +14,69 @@ import { TransactionListComponent } from './transaction-list.component';
 describe('TransactionListComponent', () => {
   let component: TransactionListComponent;
   let fixture: ComponentFixture<TransactionListComponent>;
+  let elements: DebugElement[];
+  let element: HTMLElement;
+  const transaction$ = Observable.of([{
+    _id: '0',
+    amount: 0,
+    date: '',
+    type: '',
+    description: '',
+    category: {
+      _id: '1',
+      name: '',
+      icon: '',
+      color: '',
+      user: '',
+      __v: 0,
+    },
+    user: {
+      _id: '2',
+      __v: 0,
+      email: '',
+      token: '',
+      name: '',
+      profileImage: '',
+      location: '',
+      gender: '',
+      facebookId: '',
+      twitterId: '',
+      googleId: '',
+      githubId: '',
+    },
+    __v: 0,
+    coordinates: [0, 0],
+  }, {
+    _id: '3',
+    amount: 0,
+    date: '',
+    type: '',
+    description: '',
+    category: {
+      _id: '4',
+      name: '',
+      icon: '',
+      color: '',
+      user: '',
+      __v: 0,
+    },
+    user: {
+      _id: '5',
+      __v: 0,
+      email: '',
+      token: '',
+      name: '',
+      profileImage: '',
+      location: '',
+      gender: '',
+      facebookId: '',
+      twitterId: '',
+      googleId: '',
+      githubId: '',
+    },
+    __v: 0,
+    coordinates: [0, 0],
+  }]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,6 +98,9 @@ describe('TransactionListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TransactionListComponent);
     component = fixture.componentInstance;
+
+    // Create a dummy transactionMonths.
+    component.transaction$ = transaction$;
     fixture.detectChanges();
   });
 
@@ -44,5 +111,10 @@ describe('TransactionListComponent', () => {
   it('should display an add button', () => {
     const element: HTMLElement = fixture.debugElement.query(By.css('button')).nativeElement;
     expect(element.textContent).toContain('add');
+  });
+
+  it('should display two transaction components', () => {
+    const elements = fixture.debugElement.queryAll(By.css('exp-transaction'));
+    expect(elements.length).toEqual(2);
   });
 });
--- client/src/app/modules/transactions/transactions.component.spec.ts
+++ client/src/app/modules/transactions/transactions.component.spec.ts
@@ -2,15 +2,35 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { MaterialModule } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 import { RouterTestingModule } from '@angular/router/testing';
 import { reducer } from './reducers';
 import { StoreModule } from '@ngrx/store';
 
 import { TransactionsComponent } from './transactions.component';
+import { TransactionMonth } from './models/transaction-month';
 
 describe('TransactionsComponent', () => {
   let component: TransactionsComponent;
   let fixture: ComponentFixture<TransactionsComponent>;
+  let elements: DebugElement[];
+  let element: HTMLElement;
+  const transactionMonths$ = Observable.of([{
+    _id: {
+      month: 6,
+      year: 2017,
+    }
+  }, {
+    _id: {
+      month: 7,
+      year: 2017,
+    }
+  }, {
+    _id: {
+      month: 8,
+      year: 2017,
+    }
+  }]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,10 +49,22 @@ describe('TransactionsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TransactionsComponent);
     component = fixture.componentInstance;
+
+    // Create a dummy transactionMonths.
+    component.transactionMonths$ = transactionMonths$;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the category month and year', () => {
+    elements = fixture.debugElement.queryAll(By.css('nav a'));
+    const transactionMonths = ['2017-6', '2017-7', '2017-8'];
+
+    elements.map((element, index) => {
+      expect(element.nativeElement.textContent).toContain(transactionMonths[index]);
+    });
   });
 });
