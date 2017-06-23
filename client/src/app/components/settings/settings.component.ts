a475487c7615642df0624a3c1dcd59d903737efc
nirgn975 <nir@galon.io>
2017-06-23 12:49:39 +0300

    Upgrade material version

--- client/src/app/components/settings/settings.component.ts
+++ client/src/app/components/settings/settings.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'exp-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsComponent implements OnInit {
 
